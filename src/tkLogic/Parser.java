4b53a597830fa8f1ce48113fbbbd726b94b4131c
BenedictSu <BenedictSu@users.noreply.github.com>
2014-10-02 15:52:19 +0800

    refactor parser.java class
    -first commit
    -refactor the methods call in the main method

--- src/tkLogic/Parser.java
+++ src/tkLogic/Parser.java
@@ -12,35 +12,39 @@ import tkLibrary.Task;
  * The string is all information of the command, except the command type.
  */
 public class Parser {
-	
-	private static Date parseStartTime(String userCommand) {
-		return null;	
-	}
-	
-	private static Date parseEndTime(String userCommand) {
-		return null;
-	}
-	
-	private static String parseDescription(String userCommand) {
-		return null;
-	}
-	
-	private static String parseLocation(String userCommand) {
-		return null;
-	}
-	
-	private static FrequencyType parseFrequency(String userCommand) {
-		return null;
-	}
-	
-	public static Task format(String userCommand) {
-		Task task = new Task();
-		task.setStartTime(parseStartTime(userCommand));
-		task.setEndTime(parseEndTime(userCommand));
-		task.setDescription(parseDescription(userCommand));
-		task.setLocation(parseLocation(userCommand));
-		task.setFrequency(parseFrequency(userCommand));
-		task.setState(StateType.PENDING);
-		return task;
-	}
+
+    private static Date parseStartTime(String userCommand) {
+        return null;
+    }
+
+    private static Date parseEndTime(String userCommand) {
+        return null;
+    }
+
+    private static String parseDescription(String userCommand) {
+        return null;
+    }
+
+    private static String parseLocation(String userCommand) {
+        return null;
+    }
+
+    private static FrequencyType parseFrequency(String userCommand) {
+        return null;
+    }
+
+    public static Task format(String userCommand) {
+        Task task = new Task();
+        parseAll(userCommand, task);
+        return task;
+    }
+
+    private static void parseAll(String userCommand, Task task) {
+        task.setStartTime(parseStartTime(userCommand));
+        task.setEndTime(parseEndTime(userCommand));
+        task.setDescription(parseDescription(userCommand));
+        task.setLocation(parseLocation(userCommand));
+        task.setFrequency(parseFrequency(userCommand));
+        task.setState(StateType.PENDING);
+    }
 }
