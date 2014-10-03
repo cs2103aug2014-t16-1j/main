e1a1ca8aeb7aab15d0b8bfa5140716c05132fea9
BenedictSu <Benedict_S_S@u.nus.edu>
2014-10-03 22:40:20 +0800

    add class CommandKey
    -add CommandKey for parser to check the command
    -add initial code for interpreting command

--- src/tkLogic/Parser.java
+++ src/tkLogic/Parser.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import tkLibrary.FrequencyType;
 import tkLibrary.StateType;
 import tkLibrary.Task;
+import tkLibrary.UserInput;
+import tkLibrary.CommandKey;
 
 /*
  * receive a string of command
@@ -33,23 +35,82 @@ public class Parser {
         return null;
     }
 
-    public static Task format(String userCommand) {
+    public static UserInput format(String userCommand) {
         Task task = new Task();
-        parseAll(userCommand, task);
-        return task;
+        UserInput userInput;
+
+        userInput = parseAll(userCommand, task);
+        return userInput;
     }
 
-    private static void parseAll(String userCommand, Task task) {
-        String[] userInput = splitUserInput(userCommand);
+    private static UserInput parseAll(String userCommand, Task task) {
+        String[] userInputArray = splitUserInput(userCommand);
+        UserInput userInput = new UserInput(userInputArray[0], task);
+
+        String word;
+        String command = "description";
+        for (int i = 0; i < userInputArray.length; i++) {
+            word = userInputArray[i];
+            if (word.substring(0, 1) == "-") {
+
+                // do something about the previous description
+                // store the command type instead of command string?
+                // -f from -t to -@ -o on -b by -e every
+
+                CommandKey commandKey = determineCommandType(word);
+
+                switch (commandKey) {
+                case FROM:
+                    return add(userCommand);
+                case TO:
+                    return display();
+                case AT:
+                    return delete(userCommand);
+                case ON:
+                    return clear();
+                case BY:
+                    return sort();
+                case EVERY:
+                    return search(userCommand);
+                default:
+                    throw new Error("Unrecognized command type");
+                }
+            } else {
+                word += " " + word;
+            }
+        }
+
         task.setStartTime(parseStartTime(userCommand));
         task.setEndTime(parseEndTime(userCommand));
         task.setDescription(parseDescription(userCommand));
         task.setLocation(parseLocation(userCommand));
         task.setFrequency(parseFrequency(userCommand));
         task.setState(StateType.PENDING);
+
+        return userInput;
     }
 
     public static String[] splitUserInput(String userCommand) {
         return userCommand.trim().split("\\s+");
+    }
+
+    private static CommandType determineCommandType(String commandTypeString) {
+        if (commandTypeString == null) {
+            throw new Error("command type string cannot be null!");
+        }
+
+        if (commandTypeString.equalsIgnoreCase("-a")) {
+            return CommandType.ADD;
+        } else if (commandTypeString.equalsIgnoreCase("-d")) {
+            return CommandType.DELETE;
+        } else if (commandTypeString.equalsIgnoreCase("-u")) {
+            return CommandType.UNDO;
+        } else if (commandTypeString.equalsIgnoreCase("-e")) {
+            return CommandType.EDIT;
+        } else if (commandTypeString.equalsIgnoreCase("-c")) {
+            return CommandType.CLEAR;
+        } else {
+            return CommandType.LIST;
+        }
     }
 }
--- src/tkLogic/ParserTest.java
+++ src/tkLogic/ParserTest.java
@@ -9,10 +9,10 @@ public class ParserTest {
     @Test
     public void testSplitUserInput() {
         String input =
-                "add Meeting -between 9am -to 10am -on 12 Sep 2014 -at Boardroom";
+                "add Meeting -between 9am -to 10am -on 12 Sep 2014 -@ Boardroom";
         String[] output =
-                { "add", "Meeting", "-between", "9am", "-to", "10am",
-                        "-on", "12", "Sep", "2014", "-at", "Boardroom" };
+                { "add", "Meeting", "-between", "9am", "-to", "10am", "-on",
+                        "12", "Sep", "2014", "-@", "Boardroom" };
         assertArrayEquals("Test that method splitUserInput works correctly",
                 Parser.splitUserInput(input), output);
     }
