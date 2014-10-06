3539c12a3afec3d60eba272028ef939d42d1ec75
Leslie Ng <leslieng92@gmail.com>
2014-10-06 21:51:44 +0800

    changed methods to void
    - add, delete and edit methods changed to void
    - modified code to throw exceptions if unable to add, delete or edit

--- src/tkLogic/Logic.java
+++ src/tkLogic/Logic.java
@@ -14,38 +14,36 @@ import tkLibrary.Task;
  * This maybe be a little bit more complicated so I can help Ben.
  */
 public class Logic {
-	
+
 	private String fileName;
 	private ArrayList<Task> lists;
 	private Storage storage;
-	
+
 	public Logic(String fileName) {
 		this.fileName = fileName;
 		storage = new Storage(fileName);
 	}
-	
-	public String add(Task task) throws Exception {
+
+	public void add(Task task) throws Exception {
 		if(storage.queryFreeSlot(task.getStartTime()) || storage.queryFreeSlot(task.getEndTime())){
 			try{
 				storage.store(task);
 			} catch (Exception e){
-					throw new Exception("Unable to add to TasKoord.");
+				throw new Exception("Error occurred while adding to TasKoord.");
 			}
 		}
 		else{
-			return Constants.MESSAGE_CLASHING_TIMESLOTS;
+			throw new Exception(Constants.MESSAGE_CLASHING_TIMESLOTS);
 		}
-		return Constants.MESSAGE_TASK_ADDED;
 	}
-	
-	public String edit(Task taskToBeEdited, Task editedTask) {
+
+	public void edit(Task taskToBeEdited, Task editedTask) {
 		Logic tempLogic = new Logic(fileName);
 		try{
 			tempLogic.delete(taskToBeEdited);
 			tempLogic.add(editedTask);
-			return Constants.MESSAGE_TASK_EDITED;
 		} catch (Exception e){
-			return ("Unable to edit!");
+			throw new Exception("Error occurred while editing task.");
 		}
 	}
 
@@ -53,7 +51,7 @@ public class Logic {
 		return null;
 	}
 
-	public String delete(Task task) throws Exception{
+	public void delete(Task task) throws Exception{
 		if(storage.queryTask(task.getDescription())){
 			try{
 				storage.delete(task);
@@ -62,38 +60,37 @@ public class Logic {
 			}
 		}
 		else{
-			return Constants.MESSAGE_TASK_DOES_NOT_EXIST;
+			throw new Exception(Constants.MESSAGE_TASK_DOES_NOT_EXIST);
 		}
-		return Constants.MESSAGE_TASK_DELETED;
 	}
-	
-	
+
+
 	public ArrayList<Task> list() {
 		return null;
 	}
-	
+
 	public ArrayList<Task> list(StateType state) {
 		return null;
 	}
-	
+
 	public ArrayList<Task> list(Date date) {
 		return null;
 	}
-	
+
 	public ArrayList<Task> listUpcomingTasks() {
 		return null;
 	}
-	
+
 	public ArrayList<Task> search(String keyword) {
 		return null;
-		
+
 	}
-	
+
 	public void storeCommand() {
-	    // store the last command in the storage for the method Undo
-		
+		// store the last command in the storage for the method Undo
+
 	}
-	
+
 	// this logic function is to sort an array of tasks, helpful when printing
 	public ArrayList<Task> sort(ArrayList<Task> tasks) {
 		return null;
