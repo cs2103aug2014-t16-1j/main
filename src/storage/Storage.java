40e374ffe8285c42187031cf05c7e5c642522b93
SavinVarshney <savin.varshney@gmail.com>
2014-10-09 01:14:24 +0800

    added storage file_initialization

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -18,6 +18,7 @@ public class Storage {
 	// I will tell Savin about this cache later. Now just ignore the cache.
 	private String fileName;
 	private String cacheName;
+	private static final int[] DAYS_IN_MONTHS = {31,28,31,30,31,30,31,31,30,31,30,31};
 	private static ArrayList<Task[]> days= new ArrayList<Task[]>(365);
 	
 	public Storage(String fileName) {
@@ -40,13 +41,57 @@ public class Storage {
 		
 	}
 	
-	private void initialize()throws IOException{
+	private void initialize_fromFile()throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		String line = null;
+		String[] temp; 
 		while((line = br.readLine()) != null){
-			lines.add(line.substring(EXTRACT_LINES_BEGIN_INDEX));
+			temp = line.trim().split("\\s+");
+			Date start = null;
+			Date end = null;
+			start.setDate(Integer.parseInt(temp[3]));
+			start.setMonth(Integer.parseInt(temp[4]));
+			start.setHours(Integer.parseInt(temp[5]));
+			end.setDate(Integer.parseInt(temp[6]));
+			end.setMonth(Integer.parseInt(temp[7]));
+			end.setHours(Integer.parseInt(temp[8]));
+			Task task = null;
+			task.setDescription(temp[1]);
+			task.setLocation(temp[2]);
+			task.setStartTime(start);
+			task.setEndTime(end);
+			int start_date = calculate_days(task.getStartTime().getMonth(),task.getStartTime().getDate());
+			int end_date = calculate_days(task.getEndTime().getMonth(),task.getEndTime().getDate());
+			if(start_date == end_date){
+				Task[] tp = days.get(start_date-1);
+				for(int i=task.getStartTime().getHours() - 1; i<task.getEndTime().getHours();i++){
+					tp[i] = task;
+				}
+			}else{
+				for(int i=start_date-1 ; i<end_date; i++){
+					Task[] tp = days.get(i);
+					if(i == end_date - 1){
+						for(int j=0;j<task.getEndTime().getHours();j++){
+							tp[j] = task;
+						}
+					}else{
+						for(int j=task.getStartTime().getHours();j<24;j++){
+							tp[j] = task;
+						}
+					}
+				}
+			}
 		}
 		br.close();
+	}
+	
+	private int calculate_days(int month, int day){
+		int sum = 0;
+		for(int i=0; i<month-1;i++){
+			sum = sum + DAYS_IN_MONTHS[i];
+		}
+		sum = sum + day;
+		return sum;
 	}
 
 	// load everything from the cache.
@@ -80,9 +125,11 @@ public class Storage {
 				if((!tp.equals(last)) && (!tp.equals(null))){
 					Date start = tp.getStartTime();
 					Date end = tp.getEndTime();
-					out.println((count) + ". " + tp.getDescription() + " " + tp.getLocation() + " " 
+					out.println((count) + " " + tp.getDescription() + " " + tp.getLocation() + " " 
 					+ start.getDate() + " " + start.getMonth() + " " + start.getHours() + 
 					" " + end.getDate() + " " + end.getMonth() + " "+ end.getHours());
+					count++;
+					last = tp;
 				}
 			}
 		}
