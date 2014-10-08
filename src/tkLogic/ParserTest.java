0c59a2fbd262685b4f3263d022a630ec46e12177
BenedictSu <BenedictSu@users.noreply.github.com>
2014-10-08 21:03:46 +0800

    add test casese for delete and edit
    add test cases for delete and edit

--- src/tkLogic/ParserTest.java
+++ src/tkLogic/ParserTest.java
@@ -12,7 +12,7 @@ import tkLibrary.UserInput;
 public class ParserTest {
 
     @Test
-    public void testParser() {
+    public void testParserAdd() {
         Parser parser = new Parser();
         String input = "add Meeting -f 9am -t 10am -o 12 Sep 2014 -@ Boardroom";
         UserInput userInput = parser.format(input);
@@ -36,6 +36,77 @@ public class ParserTest {
                 task.getFrequency());
         assertEquals("Test that the frequency is as default", null,
                 task.getFrequencyType());
+    }
+
+    @Test
+    public void testParserDelete() {
+        Parser parser = new Parser();
+        String input = "delete Meeting";
+        UserInput userInput = parser.format(input);
+        CommandType command = userInput.getCommand();
+        assertEquals("Test that method format works correctly",
+                CommandType.DELETE, command);
+        Task task = userInput.getTask();
+        assertEquals("Test that the Description is correctly recorded",
+                "Meeting", task.getDescription());
+        assertEquals("Test that the Start Time is correctly recorded", null,
+                task.getStartTime());
+        assertEquals("Test that the End Time is correctly recorded", null,
+                task.getEndTime());
+        assertEquals("Test that the Location is correctly recorded", null,
+                task.getLocation());
+        assertEquals("Test that the State is correctly set", StateType.PENDING,
+                task.getState());
+        assertEquals("Test that the frequency is as default", 0,
+                task.getFrequency());
+        assertEquals("Test that the frequency is as default", null,
+                task.getFrequencyType());
+    }
+
+    @Test
+    public void testParserEdit() {
+        Parser parser = new Parser();
+        String input =
+                "edit Meeting -f 9am -t 10am -o 12 Sep 2014 -@ Boardroom"
+                        + " -c Board Meeting -f 9pm -t 12pm -o 12 Dec 2014 -@ Home";
+        UserInput userInput = parser.format(input);
+        CommandType command = userInput.getCommand();
+        assertEquals("Test that method format works correctly",
+                CommandType.EDIT, command);
+        Task task = userInput.getTask();
+        assertEquals("Test that the Description is correctly recorded",
+                "Meeting", task.getDescription());
+        assertEquals("Test that the Start Time is correctly recorded",
+                "Fri Sep 12 09:00:00 SGT 2014", task.getStartTime().getTime()
+                        .toString());
+        assertEquals("Test that the End Time is correctly recorded",
+                "Fri Sep 12 10:00:00 SGT 2014", task.getEndTime().getTime()
+                        .toString());
+        assertEquals("Test that the Location is correctly recorded",
+                "Boardroom", task.getLocation());
+        assertEquals("Test that the State is correctly set", StateType.PENDING,
+                task.getState());
+        assertEquals("Test that the frequency is as default", 0,
+                task.getFrequency());
+        assertEquals("Test that the frequency is as default", null,
+                task.getFrequencyType());
+        Task editedTask = userInput.getEditedTask();
+        assertEquals("Test that the Description is correctly recorded",
+                "Board Meeting", editedTask.getDescription());
+        assertEquals("Test that the Start Time is correctly recorded",
+                "Fri Dec 12 21:00:00 SGT 2014", editedTask.getStartTime()
+                        .getTime().toString());
+        assertEquals("Test that the End Time is correctly recorded",
+                "Fri Dec 12 12:00:00 SGT 2014", editedTask.getEndTime()
+                        .getTime().toString());
+        assertEquals("Test that the Location is correctly recorded", "Home",
+                editedTask.getLocation());
+        assertEquals("Test that the State is correctly set", StateType.PENDING,
+                editedTask.getState());
+        assertEquals("Test that the frequency is as default", 0,
+                editedTask.getFrequency());
+        assertEquals("Test that the frequency is as default", null,
+                editedTask.getFrequencyType());
     }
 
 }
