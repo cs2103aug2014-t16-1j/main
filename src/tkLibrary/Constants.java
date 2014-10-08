c6f9dd1a2aa1b9761f692759d93b0007aae9a920
Leslie Ng <leslieng92@gmail.com>
2014-10-08 18:19:03 +0800

    added constants
    - added constants for edit
    - added constants for exception handling

--- src/tkLibrary/Constants.java
+++ src/tkLibrary/Constants.java
@@ -11,5 +11,14 @@ public final class Constants {
 	public static final String MESSAGE_TASK_DOES_NOT_EXIST = "Task does not exist!";
 	public static final String MESSAGE_TASK_DELETED = "Task deleted from TasKoord!";
 	public static final String MESSAGE_TASK_EDITED = "Task edited!";
-
+	public static final String MESSAGE_EDIT_TASK_DOES_NOT_EXIST = "Cannot edit because task does not exist.";
+	public static final String MESSAGE_EDIT_TASK_CLASHES = "Cannot edit because task clashes with other tasks.";
+	
+	// logic exceptions constants
+	public static final String EXCEPTIONS_ADD_FAIL = "Unable to add to TasKoord.";
+	public static final String EXCEPTIONS_DELETE_FAIL = "Unable to delete from TasKoord.";
+	public static final String EXCEPTIONS_EDIT_FAIL_BECAUSE_OF_ADD = "Edit failed because of add.";
+	public static final String EXCEPTIONS_EDIT_FAIL_BECAUSE_OF_DELETE = "Edit failed because of delete.";
+	public static final String EXCEPTIONS_EDIT_FAIL_BECAUSE_OF_OTHERS = "Edit failed because of other reason.";
+	
 }
