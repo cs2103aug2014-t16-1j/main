04b507f0b93d56fd129171bdfeec3b8033c233ae
SavinVarshney <savin.varshney@gmail.com>
2014-10-30 13:50:21 +0800

    FIx bugs in Integration Test

--- src/TasKoordIntegrationTest.java
+++ src/TasKoordIntegrationTest.java
@@ -10,10 +10,17 @@ import java.util.Scanner;
 import org.junit.Test;
 
 import storage.StorageTest;
+import storage.jsonConverter;
 import theUI.UITest;
 import theUI.UserInterface;
+import tkLibrary.Task;
+import tkLibrary.Constants;
 import tkLogic.LogicTest;
 import tkLogic.ParserTest;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class TasKoordIntegrationTest {
     private Scanner scanner;
@@ -27,39 +34,52 @@ public class TasKoordIntegrationTest {
         testDelete(ui);
     }
 
+    @SuppressWarnings("unchecked")
     private void testDelete(UserInterface ui) throws FileNotFoundException {
         ui.executeCommands("add Lunch from 10am to 11am on 25 Oct 2015 at Boardroom");
         ui.executeCommands("delete Meeting");
         readFile("IntegrationTest.txt");
-        String[] expectedData =
-                { "STARTTIME", "Oct 25 2015 10:00:00", "ENDTIME",
-                        "Oct 25 2015 11:00:00", "LOCATION", "Boardroom",
-                        "DESCRIPTION", "Lunch", "STATE_TYPE", "PENDING",
-                        "PRIORITY", "MEDIUM", "END" };
-        for (int i = 0; i < expectedData.length; i++) {
-            assertEquals(expectedData[i], scanner.nextLine());
-        }
+        JSONObject expData = new JSONObject();
+        expData.put(Constants.STARTTIME, "Oct 25 2015 10:00:00");
+        expData.put(Constants.ENDTIME, "Oct 25 2015 11:00:00");
+        expData.put(Constants.LOCATION, "Boardroom");
+        expData.put(Constants.DESCRIPTION, "Lunch");
+        expData.put(Constants.STATE_TYPE, "PENDING");
+        expData.put(Constants.PRIORITY_TYPE, "MEDIUM");
+        JSONObject actualData = readObject();
+        assertEquals(expData.get(Constants.STARTTIME).toString(),actualData.get(Constants.STARTTIME).toString());
+        assertEquals(expData.get(Constants.ENDTIME).toString(),actualData.get(Constants.ENDTIME).toString());
+        assertEquals(expData.get(Constants.LOCATION).toString(),actualData.get(Constants.LOCATION).toString());
+        assertEquals(expData.get(Constants.DESCRIPTION).toString(),actualData.get(Constants.DESCRIPTION).toString());
+        assertEquals(expData.get(Constants.STATE_TYPE).toString(),actualData.get(Constants.STATE_TYPE).toString());
+        assertEquals(expData.get(Constants.PRIORITY_TYPE).toString(),actualData.get(Constants.PRIORITY_TYPE).toString());
         assertEquals(false, scanner.hasNext());
         closeReadFile();
     }
 
+    @SuppressWarnings("unchecked")
     private void testSimpleAdd(UserInterface ui) throws FileNotFoundException {
         ui.executeCommands("add Meeting from 9am to 10am on 24 Oct 2015 at Boardroom");
         readFile("IntegrationTest.txt");
-        String[] expectedData =
-                { "STARTTIME", "Oct 24 2015 09:00:00", "ENDTIME",
-                        "Oct 24 2015 10:00:00", "LOCATION", "Boardroom",
-                        "DESCRIPTION", "Meeting", "STATE_TYPE", "PENDING",
-                        "PRIORITY", "MEDIUM", "END" };
-        for (int i = 0; i < expectedData.length; i++) {
-            assertEquals(expectedData[i], scanner.nextLine());
-        }
+        JSONObject expData = new JSONObject();
+        expData.put(Constants.STARTTIME, "Oct 24 2015 09:00:00");
+        expData.put(Constants.ENDTIME, "Oct 24 2015 10:00:00");
+        expData.put(Constants.LOCATION, "Boardroom");
+        expData.put(Constants.DESCRIPTION, "Meeting");
+        expData.put(Constants.STATE_TYPE, "PENDING");
+        expData.put(Constants.PRIORITY_TYPE, "MEDIUM");
+        JSONObject actualData = readObject();
+        assertEquals(expData.get(Constants.STARTTIME).toString(),actualData.get(Constants.STARTTIME).toString());
+        assertEquals(expData.get(Constants.ENDTIME).toString(),actualData.get(Constants.ENDTIME).toString());
+        assertEquals(expData.get(Constants.LOCATION).toString(),actualData.get(Constants.LOCATION).toString());
+        assertEquals(expData.get(Constants.DESCRIPTION).toString(),actualData.get(Constants.DESCRIPTION).toString());
+        assertEquals(expData.get(Constants.STATE_TYPE).toString(),actualData.get(Constants.STATE_TYPE).toString());
+        assertEquals(expData.get(Constants.PRIORITY_TYPE).toString(),actualData.get(Constants.PRIORITY_TYPE).toString());
         assertEquals(false, scanner.hasNext());
         closeReadFile();
     }
 
-    private void testCommandClear(UserInterface ui) throws IOException,
-            FileNotFoundException {
+    private void testCommandClear(UserInterface ui) throws IOException, FileNotFoundException {
         writeToFile("IntegrationTest.txt");
         printWriter.println("A randome message!");
         closeWrittenFile();
@@ -67,6 +87,20 @@ public class TasKoordIntegrationTest {
         readFile("IntegrationTest.txt");
         assertEquals(false, scanner.hasNext());
         closeReadFile();
+    }
+    
+    private JSONObject readObject(){
+    	JSONParser parser = new JSONParser();
+		try{
+			String line = scanner.nextLine();
+			Object obj = parser.parse(line.trim());
+			JSONObject jsonObject = (JSONObject) obj;
+			return jsonObject;
+		} catch(ParseException e){
+			System.out.println("Parse Exception:");
+			e.printStackTrace();
+			return null;
+		}
     }
 
     private void writeToFile(String fileName) throws IOException {
@@ -118,8 +152,12 @@ public class TasKoordIntegrationTest {
     }
 
     private void testStorage() {
-        //StorageTest storageTest = new StorageTest();
-        //storageTest.testStorageAdd();
+        StorageTest storageTest = new StorageTest();
+        storageTest.testStorageAdd();
+        storageTest.testStorageDeleteandUndo();
+        storageTest.testStorageEdit();
+        storageTest.testStorageSet();
+        storageTest.clear();
     }
 
 }
--- src/storage/StorageTest.java
+++ src/storage/StorageTest.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 //import java.util.Calendar;
 //import java.util.Scanner;
+
 
 //import tkLibrary.CommandType;
 //import tkLibrary.Constants;
@@ -117,18 +119,18 @@ public class StorageTest {
     @Test
     public void testStorageSet() {
         Parser parser = Parser.getInstance();
-        String input = "set Meeting from 9am to 10am on 12 Sep 2014 at Boardroom priority high status completed";
+        String input = "set Board Meeting from 9pm to 11pm on 12 Dec 2014 at Home priority high status completed";
         try {
             UserInput userInput = parser.format(input);
             Task task = userInput.getTask();
             store.set(task);
             ArrayList<Task> list = store.loadFromFile();
-            assertEquals("Test if task description was successfully set", "Meeting", list.get(0).getDescription());
-			assertEquals("Test if task description was successfully set", "Boardroom", list.get(0).getLocation());
+            assertEquals("Test if task description was successfully set", "Board Meeting", list.get(0).getDescription());
+			assertEquals("Test if task description was successfully set", "Home", list.get(0).getLocation());
 			assertEquals("Test that the Start Time is correctly recorded",
-                    "Fri Sep 12 09:00:00 SGT 2014", list.get(0).getStartTime().getTime().toString());
+                    "Fri Dec 12 21:00:00 SGT 2014", list.get(0).getStartTime().getTime().toString());
 			assertEquals("Test that the End Time is correctly recorded",
-                    "Fri Sep 12 10:00:00 SGT 2014", list.get(0).getEndTime().getTime().toString());
+                    "Fri Dec 12 23:00:00 SGT 2014", list.get(0).getEndTime().getTime().toString());
 			assertEquals("Test that the State is correctly set", StateType.COMPLETED,list.get(0).getState());
 			assertEquals("Test that the frequency is as default", 0,list.get(0).getFrequency());
 			assertEquals("Test that the frequency is as default", null,list.get(0).getFrequencyType());
@@ -138,5 +140,9 @@ public class StorageTest {
             e.printStackTrace();
             assert (false);
         }
+    }
+    
+    public void clear(){
+    	store.clear();
     }
 }
